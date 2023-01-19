e46a2342ee05f7c72302027b67a505f4351832ec
ss10ss10 <sulaiman.sultan96@gmail.com>
2023-01-19 22:06:12 +0500

    Update add_vehicles.php

--- admin/add_vehicles.php
+++ admin/add_vehicles.php
@@ -10,26 +10,22 @@ if (mysqli_connect_errno()) {
 // Check if the form has been submitted
 if (isset($_POST['make']) && isset($_POST['model']) && isset($_POST['year'])) {
     // Retrieve the form data and sanitize it
-    $make = mysqli_real_escape_string($db, $_POST['make']);
-    $model = mysqli_real_escape_string($db, $_POST['model']);
-    $year = mysqli_real_escape_string($db, $_POST['year']);
+        $make = mysqli_real_escape_string($db, $_POST['make']);
+        $model = mysqli_real_escape_string($db, $_POST['model']);
+        $year = mysqli_real_escape_string($db, $_POST['year']);
     
     // Perform form validation
     if (empty($make) || empty($model) || empty($year)) {
         echo "Make, Model and Year fields are required";
     } else {
-        echo "Here";
         // Add the vehicle to the database
-        $query = "INSERT INTO vehicles (make, model, myear) VALUES ($make, $model, $year)";
-        echo "Here 0.5";
-        $result = mysqli_query($db, $query);
-        echo "Here 1";
+        $query1 = "INSERT INTO vehicles (make, model, myear) VALUES ($make, $model, $year)";
+        $result1 = mysqli_query($db, $query) or die(mysql_error());
     }
     if(empty($result)) {
-        echo "Here 2";
-        $query = "CREATE TABLE vehicles (make varchar(255) NOT NULL, model varchar(255) NOT NULL, myear YEAR NOT NULL,)";
-        $result = mysqli_query($db, $query);
-        echo "Here 3";
+        $query2 = "CREATE TABLE vehicles (make varchar(255) NOT NULL, model varchar(255) NOT NULL, myear YEAR NOT NULL,)";
+        $result2 = mysqli_query($db, $query2) or die(mysql_error());
+        $result3 = mysqli_query($db, $query1) or die(mysql_error());
         header("Location: http://localhost/ShowroomManagementSystem/admin/inventory.php");
     }
 }
