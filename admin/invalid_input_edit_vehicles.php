988c138acd0f6e86ff8ed21649968b2c18615e5f
Sameed <87655480+UnderCoverTable@users.noreply.github.com>
2023-01-23 19:41:16 +0500

    Styled

--- admin/invalid_input_edit_vehicles.php
+++ admin/invalid_input_edit_vehicles.php
@@ -3,14 +3,22 @@
     <title>Edit Vehicles</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../stylesheets/invalid.css">
+
 
 </head>
 <body>
-    <div class="container" style="display: flex; justify-content: center;width: 60%; height: 60%; margin: auto; border: 3px solid black;">
+<div class="container">
+    <div id = "div-error">
         <h1 style="">Make, Model and Year fields are required</h1>
-        <a style="position: absolute; top: 120px; left: 610px;" href="http://localhost/ShowroomManagementSystem/admin/inventory.php">
-            <button style="width: 100px;height: 40px; border-radius: 20px; font-weight: bold;">Go Back</button>
+
+        <br>
+        <br>
+        <a href="http://localhost/ShowroomManagementSystem/admin/inventory.php">
+            <button>Go Back</button>
         </a>
     </div>
+
+</div>
 </body>
 </html>
--- customer/profile.php
+++ customer/profile.php
@@ -1,29 +1,48 @@
 <html>
 <head>
     <title>Profile</title>
+    <link rel="stylesheet" href="../stylesheets/profile.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+ 
+
 </head>
 <body>
-<div class="profile-box">
+<div class="container">
     <h2>Profile</h2>
-    <table>
-        <tr>
-            <td>Username:</td>
-            <td>
-                <?php
-                session_start();
-                echo $_SESSION['username'];
-                ?>
-            </td>
-        </tr>
-        <tr>
-            <td>Password:</td>
-            <td>
-                <?php
-                echo $_SESSION['password'];
-                ?>
-            </td>
-        </tr>
-    </table>
+
+
+
+    <div class="container">
+        
+        <h3>Username:</h3>
+
+        <h5>
+                        <?php
+                        session_start();
+                        echo $_SESSION['username'];
+                        // echo "USERNAME";
+                        ?> 
+        </h5>
+
+        <br>
+        <br>
+
+
+        <h3>Password:</h3>
+
+        <h5>
+                        <?php
+                        // session_start();
+                        echo $_SESSION['username'];
+                        // echo "PASSWORD";
+                        ?> 
+        </h5>
+
+
+    </div>
+
+
     <br>
     <div class="go-back-button">
         <a href="customer_homepage.php"><button>Go Back</button></a>
